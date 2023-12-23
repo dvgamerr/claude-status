fca483006d495cb6ed181a72797b795df47fe92c
Kananek Thongkam <info.dvgamer@gmail.com>
2023-12-23 22:21:21 +0700

    fix: else if

--- rpi.sh
+++ rpi.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-if [[ "$1" != "--blacklight" ]]; then
-  if [[ "$2" == "off" ]]; then
-    for v in 255 239 223 207 191 175 159 143 127 111 95 79 63 47 31 15 0; do
-      vcgencmd set_backlight $v;
-      sleep 0.04;
-    done;
-  else
-    for v in 0 15 31 47 63 79 95 111 127 143 159 175 191 207 223 239 255 ; do
-      vcgencmd set_backlight $v;
-      sleep 0.04;
-    done;
-  fi
-fi
-
 check_updated () {
   echo Checking \`aide-pi\` version latest...
   /usr/bin/git pull origin --quiet
@@ -24,6 +9,20 @@ cd /home/pi/lab > /dev/null
 if [ -f "/usr/bin/git" ]; then
   if [[ "$1" != "-w" ]]; then
     check_updated
+  fi
+fi
+
+if [[ "$1" == "--blacklight" ]]; then
+  if [[ "$2" == "off" ]]; then
+    for v in 255 239 223 207 191 175 159 143 127 111 95 79 63 47 31 15 0; do
+      vcgencmd set_backlight $v;
+      sleep 0.04;
+    done;
+  else
+    for v in 0 15 31 47 63 79 95 111 127 143 159 175 191 207 223 239 255 ; do
+      vcgencmd set_backlight $v;
+      sleep 0.04;
+    done;
   fi
 fi
 
