f82511f293eceb4d21c624ad41ec04889b9924c1
Kananek Thongkam <info.dvgamer@gmail.com>
2023-12-26 15:59:33 +0700

    refactor: update rpi

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/dvgamerr/aide-lab
 go 1.21
 
 require (
+	github.com/alexflint/go-arg v1.4.3
 	github.com/fatih/color v1.16.0
 	github.com/gdamore/tcell/v2 v2.7.0
 	github.com/rivo/tview v0.0.0-20231206124440-5f078138442e
 )
 
 require (
-	github.com/alexflint/go-arg v1.4.3 // indirect
-	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/alexflint/go-scalar v1.2.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- rpi.sh
+++ rpi.sh
@@ -1,43 +1,41 @@
 #!/usr/bin/zsh
+
+# Check and update the 'aide-pi' repository
 check_updated () {
   cd /home/pi/lab > /dev/null
   echo Checking \`aide-pi\` latest version...
   /usr/bin/git pull origin --quiet
 }
 
+# build the 'raspi-lab' executable
+build_raspi_lab () {
+  echo "``raspi-lab`` compiling..."
+  go get . > /dev/null
+  go build -o /home/pi/lab/bin/raspi-lab . > /dev/null
+}
+
+# Adjusts Raspberry Pi backlight intensity using vcgencmd based on command-line arguments.
 if [[ "$1" == "--blacklight" ]]; then
   if [[ "$2" == "off" ]]; then
     for v in 255 239 223 207 191 175 159 143 127 111 95 79 63 47 31 15 0; do
       vcgencmd set_backlight $v > /dev/null
-      sleep 0.02;
+      sleep 0.1;
     done;
   else
     for v in 0 15 31 47 63 79 95 111 127 143 159 175 191 207 223 239 255 ; do
       vcgencmd set_backlight $v > /dev/null
-      sleep 0.04;
+      sleep 0.2;
     done;
   fi
   exit 0
 fi
 
-  # # Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3 
-  # # -------------------------------------------------------
-  # cpu=$(</sys/class/thermal/thermal_zone0/temp)
-  # gpu=$(/usr/bin/vcgencmd measure_temp | grep -o -E '[[:digit:]].*')
-  # echo "CPU: $((cpu/1000))'C GPU: $gpu"
-  # echo "Ready, What you want to do? ..."
-
-if [[ $(tty) == /dev/tty1 ]]; then
-  if [ -f "/home/pi/lab/bin/raspi-lab" ]; then
-    if [ -f "/usr/bin/git" ]; then
-      if [[ "$1" != "-w" ]]; then
-        check_updated
-      fi
-    fi
-
-    echo "``raspi-lab`` compiling..."
-    go get . > /dev/null
-    go build -o /home/pi/lab/bin/raspi-lab . > /dev/null
-    /home/pi/lab/bin/raspi-lab --tty $(tty)
+# Conditional script to run 'raspi-lab' on the /dev/tty1 terminal.
+if [[ $(tty) == /dev/tty1 && -f "/home/pi/lab/bin/raspi-lab" ]]; then
+  if [ -f "/usr/bin/git" ]; then
+    check_updated
   fi
+
+  build_raspi_lab
+  /home/pi/lab/bin/raspi-lab --tty $(tty)
 fi
