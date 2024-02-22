fecb80f4a0bfd1b87c3a1bda93f1d6fdea9e00f0
Kananek Thongkam <info.dvgamer@gmail.com>
2024-02-22 11:43:15 +0700

    refactor: rpi with gimini

--- rpi.sh
+++ rpi.sh
@@ -1,57 +1,62 @@
 #!/bin/bash
 
+# Push directory to /home/pi/lab silently
 pushd /home/pi/lab > /dev/null
 
-url=http://localhost:21280
-rpi=raspi-lab
+# Define constants
+URL="http://localhost:21280"
+RPI="raspi-lab"
 
-# Check and update the 'aide-pi' repository
-check_updated () {
-  echo Checking \`aide-pi\` latest version...
-  /usr/bin/git checkout .
-  /usr/bin/git pull origin
-}
-
-# build the '$rpi' executable
-build_raspi_lab () {
-  cd /home/pi/lab > /dev/null
-  echo "``$rpi`` compiling..."
-  go get ./raspi-lab/ > /dev/null
-  go build -o /home/pi/lab/bin/$rpi -buildvcs=false ./raspi-lab/ > /dev/null
-}
-
-if [[ "$1" == "--update" ]]; then
-  check_updated
-  build_raspi_lab
-  exit 0
-fi
-
-if [[ "$1" == "--reload" ]]; then
+# Function to check and update the 'aide-pi' repository
+check_updated() {
+  echo "Checking \`aide-pi\` latest version..."
   git checkout .
-  curl -s -o /dev/null -X PUT $url/_exit
-  echo Signal exiting...
-fi
+  git pull origin
+}
 
-if [[ "$1" == "--run" ]]; then
-  build_raspi_lab
-  /home/pi/lab/bin/$rpi --tty $(tty)
-fi
+# Function to build the '$rpi' executable
+build_raspi_lab() {
+  cd /home/pi/lab > /dev/null
+  echo "Compiling $RPI..."
+  go get ./raspi-lab/ > /dev/null
+  go build -o /home/pi/lab/bin/$RPI -buildvcs=false ./raspi-lab/ > /dev/null
+}
 
-# Adjusts Raspberry Pi backlight intensity using vcgencmd based on command-line arguments.
-if [[ "$1" == "--blacklight" ]]; then
-  if [[ "$2" == "off" ]]; then
-    for v in 255 239 223 207 191 175 159 143 127 111 95 79 63 47 31 15 0; do
-      vcgencmd set_backlight $v > /dev/null
-      sleep 0.1;
-    done;
-  else
-    for v in 0 15 31 47 63 79 95 111 127 143 159 175 191 207 223 239 255 ; do
-      vcgencmd set_backlight $v > /dev/null
-      sleep 0.2;
-    done;
-  fi
-  exit 0
-fi
+# Handle different actions based on arguments
+case "$1" in
+  "--update")
+    check_updated
+    build_raspi_lab
+    exit 0
+    ;;
+  "--reload")
+    git checkout .
+    curl -s -o /dev/null -X PUT "$URL/_exit"
+    echo "Signal exiting..."
+    ;;
+  "--run")
+    build_raspi_lab
+    /home/pi/lab/bin/$RPI --tty $(tty)
+    ;;
+  "--blacklight")
+    if [[ "$2" == "off" ]]; then
+      for v in $(seq 255 15 -0); do
+        vcgencmd set_backlight $v > /dev/null
+        sleep 0.1
+      done
+    else
+      for v in $(seq 0 15 255); do
+        vcgencmd set_backlight $v > /dev/null
+        sleep 0.2
+      done
+    fi
+    exit 0
+    ;;
+  *)
+    echo "Invalid argument. Usage: $0 [--update, --reload, --run, --blacklight [on|off]]"
+    exit 1
+    ;;
+esac
 
 # Function to handle CTRL+C and set exit flag
 ctrl_c() {
@@ -59,30 +64,39 @@ ctrl_c() {
   eof=true
 }
 
-# Set up trap to call ctrl_c function on CTRL+C
+# Exit on errors
+if_err_exit() {
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+}
+
+# Set trap to call ctrl_c function on CTRL+C
 trap ctrl_c INT
 
 # Flag to control loop exit
 eof=false
 
-while :
-do
-  # Conditional script to run '$rpi' on the /dev/tty1 terminal.
-  if [[ $(tty) == /dev/tty1 && -f "/home/pi/lab/bin/$rpi" ]]; then
+while true; do
+  # Run script on /dev/tty1 if executable exists
+  if [[ $(tty) == /dev/tty1 && -f "/home/pi/lab/bin/$RPI" ]]; then
     check_updated
     build_raspi_lab
-    if [ $? -ne 0 ]; then
-      exit 1
-    fi
-    /home/pi/lab/bin/$rpi --tty $(tty) --db
+    if_err_exit
+
+    /home/pi/lab/bin/$RPI --tty $(tty) --db
+    if_err_exit
+
     reset
 
-    # Check if the exit flag is set
+    # Check if exit flag is set
     if [ "$eof" = true ]; then
-      break  # Exit the loop
+      break
     fi
   else
-    echo a \"$(tty)\" not is tty1 or not build raspi-lab.
+    echo "$(tty)" is not /dev/tty1 or raspi-lab not built.
     break
   fi
 done
+
+popd  # Pop directory back
